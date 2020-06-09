6a385c833b74dff90e0c33e55e51d7d917475f88
Elina <tubiselina@gmail.com>
2020-06-09 23:36:49 +0300

    start of the background

---
+++ lab3/ex4.py
@@ -0,0 +1,9 @@
+from graph import *
+
+def main():
+    background(500, 700)
+
+def background(width, height):
+    """draws background
+    width, height are width and hight of the image"""
+    pass
