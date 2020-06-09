a85d47bd4f8df4a3b7325b00067f7b96f606569d
Elina <tubiselina@gmail.com>
2020-06-10 01:10:57 +0300

    waves function finished, unnecesarry variables deleted

--- lab3/ex4.py
+++ lab3/ex4.py
@@ -11,17 +11,24 @@ def landscape(width, height):
     penSize(0)
     boarder_sky = 0.45*height
     boarder_see = 0.7*height
-    boarder_beach = height
     brushColor(0, 200, 255)
     rectangle(0, 0, width, boarder_sky) #sky
     brushColor('blue')
     rectangle(0, boarder_sky, width, boarder_see)
     brushColor('yellow')
-    rectangle(0, boarder_see, width, boarder_beach)
-    return boarder_sky, boarder_see, boarder_beach
+    rectangle(0, boarder_see, width, height)
+    return boarder_sky, boarder_see
 
-def waves(width, height, numWaves):
-    pass
+def waves(width, height, numWaves, boarder_see):
+    penSize(0)
+    dl = width/(2*numWaves)
+    position = dl/2
+    for i in range(1, numWaves + 1):
+        brushColor('yellow')
+        circle(position, boarder_see + 0.02*height, dl/2)
+        brushColor('blue')
+        circle(position + dl, boarder_see, dl/2)
+        position += 2*dl
 
 def background(width, height, numWaves):
     """draws background
@@ -29,7 +36,7 @@ def background(width, height, numWaves):
     canvasSize(width, height)
     windowSize(width, height)
 
-    boarder_sky, boarder_see, boarder_beach = landscape(width, height)
-    waves(width, height, numWaves)
+    boarder_sky, boarder_see = landscape(width, height)
+    waves(width, height, numWaves, boarder_see)
 
 main()
