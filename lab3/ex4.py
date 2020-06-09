8b2f20b6fc4239142cfa382176f99edf123c374e
Elina <tubiselina@gmail.com>
2020-06-10 00:35:32 +0300

    landscape function finished

--- lab3/ex4.py
+++ lab3/ex4.py
@@ -6,7 +6,15 @@ def main():
     run()
 
 def landscape(width, height):
-    pass
+    """"draws sky, see, beach
+    height, width - size of the landscape"""
+    penSize(0)
+    brushColor(0, 200, 255)
+    rectangle(0, 0, width, 0.45*height) #sky
+    brushColor('blue')
+    rectangle(0, 0.45*height, width, 0.7*height)
+    brushColor('yellow')
+    rectangle(0, 0.7*height, width, height)
 
 def waves(width, height, numWaves):
     pass
@@ -16,6 +24,7 @@ def background(width, height, numWaves):
     width, height are width and hight of the image"""
     canvasSize(width, height)
     windowSize(width, height)
+
     landscape(width, height)
     waves(width, height, numWaves)
 
