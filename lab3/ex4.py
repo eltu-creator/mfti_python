94ee047063c65c2ba972b391f85dd00dc271f80f
Elina <tubiselina@gmail.com>
2020-06-10 00:59:26 +0300

    background function modified

--- lab3/ex4.py
+++ lab3/ex4.py
@@ -9,12 +9,16 @@ def landscape(width, height):
     """"draws sky, see, beach
     height, width - size of the landscape"""
     penSize(0)
+    height_sky = 0.45*height
+    height_see = 0.7*height
+    height_beach = height
     brushColor(0, 200, 255)
-    rectangle(0, 0, width, 0.45*height) #sky
+    rectangle(0, 0, width, height_sky) #sky
     brushColor('blue')
-    rectangle(0, 0.45*height, width, 0.7*height)
+    rectangle(0, height_sky, width, height_see)
     brushColor('yellow')
-    rectangle(0, 0.7*height, width, height)
+    rectangle(0, height_see, width, height_beach)
+    return height_sky, height_see, height_beach
 
 def waves(width, height, numWaves):
     pass
@@ -25,7 +29,8 @@ def background(width, height, numWaves):
     canvasSize(width, height)
     windowSize(width, height)
 
-    landscape(width, height)
+    height_sky, height_see, height_beach = landscape(width, height)
+    print(height_sky, height_see, height_beach)
     waves(width, height, numWaves)
 
 main()
